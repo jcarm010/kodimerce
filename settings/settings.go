6f0b2699dd90f9904775e6c96b1a1b3e2e580406
Javier Carmona <jcarm010@fiu.edu>
2016-11-19 12:55:40 -0500

    removed page bodies and added paypal keys

--- settings/settings.go
+++ settings/settings.go
@@ -2,4 +2,8 @@ package settings
 
 const (
 	COMPANY_NAME = "MandalaCrame"
+	PAYPAL_API_URL = "https://api.sandbox.paypal.com/v1/"
+	PAYPAL_ACCOUNT = "jcarmona0317-facilitator@hotmail.com"
+	PAYPAL_API_CLIENT_ID = "AcwbfBP-fH9gKOmvsiKexVLZezNhhr44ha5rtximHSRtEDX61wu7URITF1xJV9LEzGZ1zrM_Stxj6kHs"
+	PAYPAL_API_CLIENT_SECRET = "EF_YKPVR4qxeo_skx_jG64kh0W0Q7jMTy8tRvxGQEA696AaHMVilWS7Im5cyF6G5GNgzJxIBDd4e0-X2"
 )
