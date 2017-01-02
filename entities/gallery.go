5705ee364663f1f9b591f700618445eb16c350c6
Javier Carmona <jcarm010@fiu.edu>
2017-01-02 18:10:53 -0500

    fixed bug reading blobs from datastore

--- entities/gallery.go
+++ entities/gallery.go
@@ -5,6 +5,7 @@ import (
 	"google.golang.org/appengine/datastore"
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
+	"strings"
 )
 
 const ENTITY_BLOB = "__BlobInfo__"
@@ -13,12 +14,15 @@ func ListUploads(ctx context.Context) ([]*blobstore.BlobInfo, error) {
 	blobs := make([]*blobstore.BlobInfo, 0)
 	keys, err := datastore.NewQuery(ENTITY_BLOB).GetAll(ctx, &blobs)
 	if err != nil {
-		return nil, err
+		index := strings.Index(err.Error(), "datastore: cannot load field")
+		if index != 0 {
+			return nil, err
+		}
 	}
 
 	for i, blob := range blobs {
 		blob.BlobKey = appengine.BlobKey(keys[i].StringID())
 	}
 
-	return blobs, err
+	return blobs, nil
 }
