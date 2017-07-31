583c00f1d1a6650479272859d397475e57aebd00
Javier Carmona <jcarm010@fiu.edu>
2017-07-31 08:44:34 -0400

    added twitter, linkedin, youtube, contact email, and contact phone to views

--- view/view.go
+++ view/view.go
@@ -12,11 +12,16 @@ type View struct {
 	Keywords string
 	CompanyName string
 	CompanyNameAlternate string
+	ContactEmail string
+	ContactPhone string
 	CompanyUrl string
 	CanonicalUrl string
 	PageUrl string
 	FacebookUrl string
 	InstagramUrl string
+	TwitterUrl string
+	LinkedInUrl string
+	YouTubeUrl string
 }
 
 func NewView(request *http.Request, title string, metaDescription string) *View {
@@ -35,10 +40,15 @@ func NewView(request *http.Request, title string, metaDescription string) *View 
 		MetaDescription: metaDescription,
 		CompanyName: settings.COMPANY_NAME,
 		CompanyNameAlternate: settings.COMPANY_NAME_ALTERNATE,
+		ContactEmail: settings.COMPANY_CONTACT_EMAIL,
+		ContactPhone: settings.COMPANY_CONTACT_PHONE,
 		CompanyUrl: settings.ServerUrl(request),
 		CanonicalUrl: newUrl,
 		PageUrl: settings.ServerUrl(request) + request.URL.String(),
 		FacebookUrl: settings.FACEBOOK_URL,
 		InstagramUrl: settings.INSTAGRAM_URL,
+		TwitterUrl: settings.TWITTER_URL,
+		LinkedInUrl: settings.LINKEDIN_URL,
+		YouTubeUrl: settings.YOUTUBE_URL,
 	}
 }
